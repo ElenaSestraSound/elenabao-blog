27c1db42898c327aa12100d61990de469eb640fe
elena.sestrasound <elena.sestrasound@gmail.com>
2023-03-21 17:29:02 +0100

    basic routing

---
+++ src/pages/posts/[slug]/index.tsx
@@ -0,0 +1,5 @@
+function SinglePostPage() {
+    return <h1>Single Post Page</h1>
+}
+
+export default SinglePostPage
---
+++ src/pages/posts/index.tsx
@@ -0,0 +1,5 @@
+function AllPostsPage() {
+    return <h1>All posts Page</h1>
+}
+
+export default AllPostsPage
