9d0fd8bc4c6ce63913383d7ac0e53f5f95bae276
elena.sestrasound <elena.sestrasound@gmail.com>
2023-04-04 14:18:38 +0200

    union type error in chackraUI library solved

--- frontend/src/components/design-system/layout/side-layout/index.tsx
+++ frontend/src/components/design-system/layout/side-layout/index.tsx
@@ -12,14 +12,14 @@ export default function SideLayout({ children, background }: ISideLayoutProps) {
     const arrayChildren = React.Children.toArray(children)
     return (
         <Layout background={background}>
-            <Flex css={sideLayout}>
+            <Box display='flex' css={sideLayout}>
                 <Box w='70%'>
                     {arrayChildren[0]}
                 </Box>
                 <Box w='30%'>
                     {arrayChildren[1]}
                 </Box>
-            </Flex>
+            </Box>
         </Layout>
     );
 }
