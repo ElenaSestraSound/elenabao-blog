8ac5e7f8fc19420bd595b4e23960a54b713f975e
elena.sestrasound <elena.sestrasound@gmail.com>
2023-04-11 16:36:15 +0200

    hidden search message when no outcome and made a form

--- frontend/src/components/search/index.tsx
+++ frontend/src/components/search/index.tsx
@@ -8,6 +8,7 @@ import useHttp from '@/hooks/use-http';
 
 
 export default function Search() {
+    const [isSearched, setIsSearched] = useState(false)
     const [searchValue, setSearchValue] = useState("")
     const [searchResult, setSearchResult] = useState<PostModel[]>([])
     const searchOnChange = (event: React.FormEvent<HTMLInputElement>) => {
@@ -15,7 +16,8 @@ export default function Search() {
     }
 
     const { isLoading, hasError, sendRequest } = useHttp()
-    const fetchPosts = () => {
+    const fetchPosts = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
         sendRequest({
             url: '/api/search',
             method: 'POST',
@@ -26,37 +28,38 @@ export default function Search() {
         },
             (posts: PostModel[]) => {
                 setSearchResult(posts)
+                setIsSearched(true)
             })
     }
 
-    return (<>
-        <Box textAlign='center' mb='40px'>
-            <Input
-                w='50%'
-                minW='200px'
-                backgroundColor='brand.backgroundTerciary'
-                onChange={searchOnChange}
-                placeholder='Type here...' />
-            <Button
-                type='submit'
-                ml='10px'
-                verticalAlign='baseline'
-                color='brand.textTerciary'
-                bgColor='brand.accent'
-                onClick={fetchPosts}>
-                Search</Button>
-        </Box>
-        {hasError && <Text color='red' textAlign='center' fontWeight={600}>{hasError}</Text>}
-        <Box maxW='900px' m='0 auto'>
-            {isLoading && <Box w='fit-content' m='0 auto'><Spinner color='brand.highlight' /></Box>}
-            {searchResult.length > 0 ?
-                <Feed posts={searchResult} /> :
-                <Text textAlign='center' fontWeight={600} color='brand.textSecondary'>
-                    Unable to find posts with these search parameters
-                </Text>
-            }
-        </Box>
-    </>
+    return (
+        <form onSubmit={fetchPosts}>
+            <Box textAlign='center' mb='40px'>
+                <Input
+                    w='50%'
+                    minW='200px'
+                    backgroundColor='brand.backgroundTerciary'
+                    onChange={searchOnChange}
+                    placeholder='Type here...' />
+                <Button
+                    type='submit'
+                    ml='10px'
+                    verticalAlign='baseline'
+                    color='brand.textTerciary'
+                    bgColor='brand.accent'>
+                    Search</Button>
+            </Box>
+            {hasError && <Text color='red' textAlign='center' fontWeight={600}>{hasError}</Text>}
+            <Box maxW='900px' m='0 auto'>
+                {isLoading && <Box w='fit-content' m='0 auto'><Spinner color='brand.highlight' /></Box>}
+                {searchResult.length <= 0 && isSearched ?
+                    <Text textAlign='center' fontWeight={600} color='brand.textSecondary'>
+                        Unable to find posts with these search parameters
+                    </Text> :
+                    <Feed posts={searchResult} />
+                }
+            </Box>
+        </form>
     );
 }
 
